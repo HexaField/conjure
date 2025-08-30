36fb3ab9149a3682bc6770a54df3c09cfccb5032
HexaField <10372036+HexaField@users.noreply.github.com>
2025-08-30 11:16:56 +1000

    simplify useSpawnAvatar

--- src/world/useSpawnAvatar.tsx
+++ src/world/useSpawnAvatar.tsx
@@ -1,28 +1,26 @@
-import { EntityID, SourceID, UUIDComponent } from '@ir-engine/ecs'
+import { EngineState, EntityID, SourceID, UUIDComponent } from '@ir-engine/ecs'
 import { AvatarNetworkAction } from '@ir-engine/engine/src/avatar/state/AvatarNetworkActions'
 import { dispatchAction, getState } from '@ir-engine/hyperflux'
 import { DomainConfigState } from '@ir-engine/spatial/src/resources/DomainConfigState'
 import { useEffect } from 'react'
-import { AgentState } from '../ad4m/useADAM'
 
 import '@ir-engine/engine/src/avatar/AvatarModule'
 import { AvatarComponent } from '@ir-engine/engine/src/avatar/components/AvatarComponent'
 
-export const useSpawnAvatar = (neighbourhood: string) => {
+export const useSpawnAvatar = (sceneID: string) => {
   useEffect(() => {
-    const agent = getState(AgentState)
     dispatchAction(
       AvatarNetworkAction.spawn({
-        parentUUID: UUIDComponent.join({ entitySourceID: 'root' as SourceID, entityID: neighbourhood as EntityID }),
+        parentUUID: UUIDComponent.join({ entitySourceID: 'root' as SourceID, entityID: sceneID as EntityID }),
         avatarURL:
           getState(DomainConfigState).cloudDomain +
           '/projects/enchantmentengine/default-project/assets/avatars/irRobot.vrm',
-        entitySourceID: agent!.did! as SourceID,
+        entitySourceID: getState(EngineState).userID as string as SourceID,
         entityID: AvatarComponent.entityID,
         name: 'My Avatar' /** @todo get name, maybe from flux? */
       })
     )
-  })
+  }, [])
 
   return null
 }
