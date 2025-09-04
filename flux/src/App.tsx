c3b70f5000c1c41f875254d0dd9592a51850b899
HexaField <10372036+HexaField@users.noreply.github.com>
2025-09-04 10:32:36 +1000

    tweak scene

--- flux/src/App.tsx
+++ flux/src/App.tsx
@@ -6,15 +6,30 @@ import { startTimer } from '@ir-engine/spatial/src/startTimer'
 
 import { PerspectiveProxy } from '@coasys/ad4m'
 import { AgentClient } from '@coasys/ad4m/lib/src/agent/AgentClient'
-import { createEngine, EngineState } from '@ir-engine/ecs'
-import { getMutableState, useMutableState, type UserID } from '@ir-engine/hyperflux'
-import { ReferenceSpaceState } from '@ir-engine/spatial'
+import {
+  createEngine,
+  EngineState,
+  EntityID,
+  getComponent,
+  setComponent,
+  SourceID,
+  UUIDComponent
+} from '@ir-engine/ecs'
+import { dispatchAction, getMutableState, getState, useMutableState, type UserID } from '@ir-engine/hyperflux'
+import { ReferenceSpaceState, TransformComponent } from '@ir-engine/spatial'
 import { useSpatialEngine } from '@ir-engine/spatial/src/initializeEngine'
 import { useEngineCanvas } from '@ir-engine/spatial/src/renderer/functions/useEngineCanvas'
 import React, { useEffect, useRef, useState } from 'react'
+import { SceneState } from '@ir-engine/engine/src/gltf/GLTFState'
+import { LocationState } from '@ir-engine/client-core/src/social/services/LocationService'
+import { SpectateActions } from '@ir-engine/spatial/src/camera/systems/SpectateSystem'
 
-import { useBasicScene } from '../../src/world/BasicScene'
-import { useSpawnAvatar } from '../../src/world/useSpawnAvatar'
+import { useTestScene } from '../../src/world/TestScene'
+import { CameraOrbitComponent } from '@ir-engine/spatial/src/camera/components/CameraOrbitComponent'
+import { InputComponent } from '@ir-engine/spatial/src/input/components/InputComponent'
+import { Box3, Vector3 } from 'three'
+// import { useBasicScene } from '../../src/world/BasicScene'
+// import { useSpawnAvatar } from '../../src/world/useSpawnAvatar'
 
 createEngine()
 startTimer()
@@ -26,8 +41,29 @@ type Props = {
 }
 
 const Scene = (props: { url: string }) => {
-  useBasicScene(props.url)
-  useSpawnAvatar(props.url)
+  useTestScene(props.url)
+  // useBasicScene(props.url)
+  // useSpawnAvatar(props.url)
+  useEffect(() => {
+    const viewer = getState(ReferenceSpaceState).viewerEntity
+    setComponent(viewer, CameraOrbitComponent)
+    setComponent(viewer, InputComponent)
+
+    const transform = getComponent(viewer, TransformComponent)
+    transform.rotation.setFromAxisAngle(new Vector3(0, 1, 0), -Math.PI / 2)
+
+    CameraOrbitComponent.setFocus(
+      viewer,
+      new Vector3(0, 1.5, 0),
+      new Box3().setFromCenterAndSize(new Vector3(0, 1.5, 0), new Vector3(3, 3, 3))
+    )
+
+    const sceneURL = `${props.url}.gltf`
+    getMutableState(LocationState).currentLocation.location.sceneURL.set(sceneURL)
+    return () => {
+      getMutableState(LocationState).currentLocation.location.sceneURL.set('')
+    }
+  }, [])
   return null
 }
 
@@ -42,13 +78,12 @@ const Engine = (props: Props) => {
   return (
     <>
       <div ref={ref} style={{ width: '100%', height: '100%', position: 'absolute' }} />
-      <div>{viewerEntity && <Scene url={props.perspective.sharedUrl!} />}</div>
+      {viewerEntity && <Scene url={props.perspective.sharedUrl!} />}
     </>
   )
 }
 
 export default function App({ agent, perspective, source }: Props) {
-  console.log({ agent, perspective, source })
   if (!perspective?.uuid || !agent) return <div>"No perspective or agent client"</div>
 
   const [ready, setReady] = useState(false)
@@ -65,12 +100,13 @@ export default function App({ agent, perspective, source }: Props) {
     <div
       style={{
         width: '100%',
-        height: '100%',
+        height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         overflow: 'hidden'
       }}
     >
+      {ready && <Engine agent={agent} perspective={perspective} source={source} />}
       <canvas
         id="engine-renderer-canvas"
         style={{
@@ -84,7 +120,6 @@ export default function App({ agent, perspective, source }: Props) {
           userSelect: 'none'
         }}
       />
-      {ready && <Engine agent={agent} perspective={perspective} source={source} />}
     </div>
   )
 }
